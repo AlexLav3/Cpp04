f83e8186afca90ebf2b9a17717dd609fdd717694
Alex <alexlav.elly@gmail.com>
2025-12-03 19:05:06 +0100

    main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,6 +2,12 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
+/*
+In your main function, create and fill an array of Animal objects. Half of it will
+be Dog objects and the other half will be Cat objects. At the end of your program
+execution, loop over this array and delete every Animal. You must delete directly dogs
+and cats as Animals.
+*/
 int main()
 {
     //given part
@@ -9,6 +15,16 @@ int main()
     const Animal* i = new Cat();
     delete j;//should not create a leak
     delete i;
-    
+    const Animal* anims[4];
+    for(int i = 0; i < 4; ++i){
+        if(i % 2 == 0)
+            anims[i] = new Dog();
+        else 
+            anims[i] = new Cat();
+    }
+    // for(int i = 0; i < 4; ++i) //just for test 
+    //     anims[i]->makeSound();
+    for(int i = 0; i < 4; ++i)
+        delete anims[i];
     return 0;
 }
