9171efdd4921bf3bc362c7058a7037d2d80036e1
Alex <alexlav.elly@gmail.com>
2026-03-03 19:26:14 +0100

    deep copy

--- ex01/Dog.cpp
+++ ex01/Dog.cpp
@@ -11,13 +11,21 @@ Dog::~Dog(){
 }
 
 Dog::Dog(const Dog &copy){
+    std::cout << "Dog copy constructor called" << std::endl;
     type = copy.type;
+    brain = new Brain(*copy.brain);
 }
 
 Dog &Dog::operator=(const Dog &other){
-	if (this != &other)
-		type = other.type;
-	return (*this);
+	std::cout << "Dog copy assignment called" << std::endl;
+    if (this != &other)
+    {
+        type = other.type;
+		if(this->brain != NULL)
+        	delete brain;
+        this->brain = new Brain(*other.brain);
+    }
+    return *this;
 }
 
 void Dog::makeSound()const {
